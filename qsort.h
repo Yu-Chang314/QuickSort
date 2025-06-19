df6ed89b736e5f7cd26e4162e547a1eac2130eea
Yu-Chang314 <eason7326@gmail.com>
2025-06-19 22:36:49 +0800

    Update qsort.h

--- qsort.h
+++ qsort.h
@@ -38,14 +38,14 @@ heap_sort(RandomAccessIterator first,
           RandomAccessIterator last,
           Compare& comp)
 {
-	typedef typename std::iterator_traits<RandomAccessIterator>::difference_type difference_type;
-	const difference_type len = last - first;
-	__builtin_assume(len >= 2);
-	for (difference_type idx = len + (len >> 1); idx > 0; --idx)
-	{
-		difference_type sift_idx = idx >= len ? idx - len : (std::iter_swap(first, first + idx), 0);
-		sift_down(first, first + std::min(idx, len), comp, sift_idx);
-	}
+    typedef typename std::iterator_traits<RandomAccessIterator>::difference_type difference_type;
+    const difference_type len = last - first;
+    __builtin_assume(len >= 2);
+    for (difference_type idx = len + (len >> 1); idx > 0; --idx)
+    {
+        difference_type sift_idx = idx >= len ? idx - len : (std::iter_swap(first, first + idx), 0);
+        sift_down(first, first + std::min(idx, len), comp, sift_idx);
+    }
 }
 
 template <class Compare,
@@ -57,8 +57,8 @@ median_of_three(RandomAccessIterator a,
                 Compare& comp)
 {
 
-	bool x = comp(*a, *b);
-	bool y = comp(*a, *c);
+    bool x = comp(*a, *b);
+    bool y = comp(*a, *c);
 
     // If x=y=0 then b, c <= a. In this case we want to return max(b, c).
     // If x=y=1 then a < b, c. In this case we want to return min(b, c).
@@ -68,7 +68,7 @@ median_of_three(RandomAccessIterator a,
         bool z = comp(*b, *c);
         return (z ^ x) ? c : b;
     }
-	return a;
+    return a;
 }
 
 template <class Compare,
@@ -81,16 +81,16 @@ median_of_three_recursive(RandomAccessIterator a,
                           Compare& comp,
                           DistanceType step)
 {
-	if ((step << 3) >= PSEUDO_MEDIAN_REC_THRESHOLD)
-	{
-		const DistanceType next_step   = step >> 3;
-		const DistanceType four_steps  = next_step << 2;
-		const DistanceType seven_steps = next_step * 7;
-		a = median_of_three_recursive(a, a + four_steps, a + seven_steps, comp, next_step);
-		b = median_of_three_recursive(b, b + four_steps, b + seven_steps, comp, next_step);
-		c = median_of_three_recursive(c, c + four_steps, c + seven_steps, comp, next_step);
-	}
-	return median_of_three(a, b, c, comp);
+    if ((step << 3) >= PSEUDO_MEDIAN_REC_THRESHOLD)
+    {
+        const DistanceType next_step   = step >> 3;
+        const DistanceType four_steps  = next_step << 2;
+        const DistanceType seven_steps = next_step * 7;
+        a = median_of_three_recursive(a, a + four_steps, a + seven_steps, comp, next_step);
+        b = median_of_three_recursive(b, b + four_steps, b + seven_steps, comp, next_step);
+        c = median_of_three_recursive(c, c + four_steps, c + seven_steps, comp, next_step);
+    }
+    return median_of_three(a, b, c, comp);
 }
 
 template <class Compare,
@@ -100,13 +100,13 @@ choose_pivot(RandomAccessIterator first,
              RandomAccessIterator last,
              Compare& comp)
 {
-	typedef typename std::iterator_traits<RandomAccessIterator>::difference_type difference_type;
-	const difference_type len  = last - first;
-	const difference_type step = len >> 3;
-	RandomAccessIterator mid = len < PSEUDO_MEDIAN_REC_THRESHOLD
-		? median_of_three(first, first + (step << 2), first + step * 7, comp)
-		: median_of_three_recursive(first, first + (step << 2), first + step * 7, comp, step);
-	std::iter_swap(first, mid);
+    typedef typename std::iterator_traits<RandomAccessIterator>::difference_type difference_type;
+    const difference_type len  = last - first;
+    const difference_type step = len >> 3;
+    RandomAccessIterator mid = len < PSEUDO_MEDIAN_REC_THRESHOLD
+        ? median_of_three(first, first + (step << 2), first + step * 7, comp)
+        : median_of_three_recursive(first, first + (step << 2), first + step * 7, comp, step);
+        std::iter_swap(first, mid);
 }
 
 template <class Compare,
@@ -116,12 +116,12 @@ hoare_branchy_cyclic(RandomAccessIterator first,
                      RandomAccessIterator last,
                      Compare& comp)
 {
-	typedef typename std::iterator_traits<RandomAccessIterator>::value_type value_type;
-	value_type pivot(std::move(*first));
-	RandomAccessIterator begin = first;
+    typedef typename std::iterator_traits<RandomAccessIterator>::value_type value_type;
+    value_type pivot(std::move(*first));
+    RandomAccessIterator begin = first;
 
-	while (++first != last && comp(*first, pivot));
-	while (first != last && !comp(*--last, pivot));
+    while (++first != last && comp(*first, pivot));
+    while (first != last && !comp(*--last, pivot));
 
     if (first < last)
     {
@@ -370,20 +370,20 @@ partition_by_choosed_pivot(RandomAccessIterator first,
                            RandomAccessIterator last,
                            Compare&& comp)
 {
-	if constexpr (use_branchless_sort<RandomAccessIterator, Compare>)
+    if constexpr (use_branchless_sort<RandomAccessIterator, Compare>)
         return bitset_partition(first, last, comp);
-	else
-		return hoare_branchy_cyclic(first, last, comp);
+    else
+        return hoare_branchy_cyclic(first, last, comp);
 }
 
 template <class Compare>
 struct reverse_predicate
 {
-	Compare& comp;
-	template <class Tp1, class Tp2>
-	[[nodiscard]] constexpr auto
-	operator()(Tp1&& left, Tp2&& right) const
-	{ return !comp(right, left); }
+    Compare& comp;
+    template <class Tp1, class Tp2>
+    [[nodiscard]] constexpr auto
+    operator()(Tp1&& left, Tp2&& right) const
+    { return !comp(right, left); }
 };
 
 template <class Compare,
@@ -395,49 +395,49 @@ quick_sort(RandomAccessIterator first,
            typename std::iterator_traits<RandomAccessIterator>::difference_type depth_limit,
            typename std::iterator_traits<RandomAccessIterator>::pointer ancestor_pivot = nullptr)
 {
-	for (;;)
-	{
+    for (;;)
+    {
         // smallsort is faster for small array.
-		if (last - first <= SSORT_MAX)
-		{
-		    small_sort(first, last, comp);
-			return;
-		}
+        if (last - first <= SSORT_MAX)
+        {
+             small_sort(first, last, comp);
+            return;
+        }
 
-		// if too many bad pivots choices we made, simply fall back to heapsort
-		// to guarantee O(nlogn) worst case.
-		if (depth_limit == 0)
-		{
-			heap_sort(first, last, comp);
-			return;
-		}
+        // if too many bad pivots choices we made, simply fall back to heapsort
+        // to guarantee O(nlogn) worst case.
+        if (depth_limit == 0)
+        {
+            heap_sort(first, last, comp);
+            return;
+        }
 
-		--depth_limit; // allow 2log2(n) divisions.
+        --depth_limit; // allow 2log2(n) divisions.
 
         // calculate the approximate median of 3 elements by median of 3 or
         // recursively from an approximation of each, if they're large enough.
         // this algorithm is taken from glidesort by Orson Peters.
-		choose_pivot(first, last, comp);
+        choose_pivot(first, last, comp);
 
         // if the chosen pivot is equal to the predecessor, we change the strategy,
         // putting the equal elements in the left partition, greater elements in
         // the right partition.
-		if (ancestor_pivot && !comp(*ancestor_pivot, *first))
-		{
-			first = partition_by_choosed_pivot(first, last, reverse_predicate{comp});
-			ancestor_pivot = nullptr;
-			++first;
-			continue;
-		}
+        if (ancestor_pivot && !comp(*ancestor_pivot, *first))
+        {
+            first = partition_by_choosed_pivot(first, last, reverse_predicate{comp});
+            ancestor_pivot = nullptr;
+            ++first;
+            continue;
+        }
 
-		RandomAccessIterator mid = partition_by_choosed_pivot(first, last, comp);
+        RandomAccessIterator mid = partition_by_choosed_pivot(first, last, comp);
         __builtin_assume(mid < last);
         // sort the left partition first using recursion and do tail recursion elimination for
         // the right-hand partition.
-		quick_sort(first, mid, comp, depth_limit, ancestor_pivot);
-		ancestor_pivot = std::to_address(mid);
-		first = ++mid;
-	}
+        quick_sort(first, mid, comp, depth_limit, ancestor_pivot);
+        ancestor_pivot = std::to_address(mid);
+        first = ++mid;
+    }
 }
 
 template <class Compare,
@@ -447,29 +447,29 @@ find_existing_run(RandomAccessIterator first,
                   RandomAccessIterator last,
                   Compare& comp)
 {
-	if (last - first < 2) // 0 or 1 element is sorted
-		return std::pair<RandomAccessIterator, bool>(last, false);
+    if (last - first < 2) // 0 or 1 element is sorted
+        return std::pair<RandomAccessIterator, bool>(last, false);
 
-	RandomAccessIterator mid = first;
-	bool is_strictly_descending = comp(*++mid, *first);
-	++mid;
+    RandomAccessIterator mid = first;
+    bool is_strictly_descending = comp(*++mid, *first);
+    ++mid;
 
-	if (is_strictly_descending)
-		while (mid != last && comp(*mid, *prev_iter(mid)))
+    if (is_strictly_descending)
+        while (mid != last && comp(*mid, *prev_iter(mid)))
+            ++mid;
+    else
+        while (mid != last && !comp(*mid, *prev_iter(mid)))
 			++mid;
-	else
-		while (mid != last && !comp(*mid, *prev_iter(mid)))
-			++mid;
-	return std::pair<RandomAccessIterator, bool>(mid, is_strictly_descending);
+    return std::pair<RandomAccessIterator, bool>(mid, is_strictly_descending);
 }
 
 template <class Tp>
 inline constexpr Tp log2i(Tp val) noexcept
 {
 #if __cplusplus >= 201703L
-	return std::bit_width(std::make_unsigned_t<Tp>(val)) - 1;
+    return std::bit_width(std::make_unsigned_t<Tp>(val)) - 1;
 #else
-	 const int sz = sizeof(+val);
+    const int sz = sizeof(+val);
     int w = sz * __CHAR_BIT__ - 1;
     if (sz == sizeof(long long))
         w -= __builtin_clzll(+val);
@@ -487,32 +487,32 @@ qsort(const RandomAccessIterator first,
 	  const RandomAccessIterator last,
 	  Compare comp)
 {
-	const auto [mid, descending] = find_existing_run(first, last, comp);
+    const auto [mid, descending] = find_existing_run(first, last, comp);
 
-	if (mid == last) // strictly ascending ==> no operation
-	{
+    if (mid == last) // strictly ascending ==> no operation
+    {
 		if (descending) // strictly descending ==> reverse
 			std::reverse(first, last);
 		return;
-	}
+    }
 	else if (mid - first >= last - mid) // first half are sorted, sort last half and merge them
-	{
-		if (descending)
-			std::reverse(first, mid);
-		quick_sort(mid, last, comp, log2i(last - mid) << 1);
-		std::inplace_merge(first, mid, last, comp);
-		return;
-	}
-    quick_sort(first, last, comp, log2i(last - first) << 1);
+    {
+        if (descending)
+            std::reverse(first, mid);
+        quick_sort(mid, last, comp, log2i(last - mid) << 1);
+        std::inplace_merge(first, mid, last, comp);
+        return;
+     }
+     quick_sort(first, last, comp, log2i(last - first) << 1);
 }
 
 template <class RandomAccessIterator>
 CONSTEXPR_CPP20 inline void
 qsort(const RandomAccessIterator first,
-	  const RandomAccessIterator last)
+      const RandomAccessIterator last)
 {
     typedef typename std::iterator_traits<RandomAccessIterator>::value_type value_type;
-	qsort(first, last, std::less<value_type>{});
+    qsort(first, last, std::less<value_type>{});
 }
 SORTER_END
 
